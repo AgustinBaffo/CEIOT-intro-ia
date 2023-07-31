7805c130c1ae9776b7365f926fc1a36b16205019
AgustinBaffo <abaffo@irobot.com>
2023-07-31 11:35:01 -0300

    use validation in gd

--- tp_integrador_enunciado/utils/data_manipulation.py
+++ tp_integrador_enunciado/utils/data_manipulation.py
@@ -75,9 +75,52 @@ def generate_outliers(
                 )
         else:
             # Generate outliers throughout the distribution
-            indices = np.random.choice(df.index, size=num_outliers, replace=True)
+            indices = np.random.choice(
+                df.index, size=num_outliers, replace=True)
             outlier_df.loc[indices, column] = np.random.uniform(
                 df[column].min(), df[column].max(), size=num_outliers
             )
 
     return outlier_df
+
+
+def data_split(X_input,
+               Y_input,
+               val_size=0.15,
+               test_size=0.15,
+               random_state=42,
+               shuffle=True):
+
+    _X_input = np.copy(X_input)
+    _Y_input = np.copy(Y_input)
+
+    if not _X_input.shape[0] == _Y_input.shape[0]:
+        raise ValueError(
+            "Los datos (X_input, Y_input) tienen distintas longitudes.")
+
+    train_size = 1 - test_size - val_size
+
+    if (train_size < 0):
+        raise ValueError(
+            "El porcentaje de datos de validacion y test no puede ser mayor al 100%.")
+
+    # Mezclar los datos de manera tal que ambos conserven los mismos indices
+    if (shuffle):
+        np.random.seed(random_state)
+        ran_idx = np.random.permutation(len(_X_input))
+        _X_input = _X_input[ran_idx]
+        _Y_input = _Y_input[ran_idx]
+
+    total_len = _X_input.shape[0]
+    train_len = int(train_size*total_len)
+    val_len = int(val_size*total_len)
+
+    X_train = np.array(_X_input[0:train_len])
+    X_val = np.array(_X_input[train_len:train_len+val_len])
+    X_test = np.array(_X_input[train_len+val_len:total_len])
+
+    Y_train = np.array(_Y_input[0:train_len])
+    Y_val = np.array(_Y_input[train_len:train_len+val_len])
+    Y_test = np.array(_Y_input[train_len+val_len:total_len])
+
+    return X_train, X_val, X_test, Y_train, Y_val, Y_test
