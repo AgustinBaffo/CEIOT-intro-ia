f722d9d60160efc7b2b2b4b3b2a9ed0fdd917b61
AgustinBaffo <abaffo@irobot.com>
2023-08-24 22:02:31 -0300

    end tp2

--- tp_integrador_enunciado/utils/data_generation.py
+++ tp_integrador_enunciado/utils/data_generation.py
@@ -12,13 +12,14 @@ def generate_dataset(n_samples: int = 1000,
                      bias: float = 0.0,
                      random_state: Optional[int] = None,
                      effective_rank: Optional[int] = None,
-                     output: str = 'dataframe') -> Union[pd.DataFrame,np.ndarray]:
+                     output: str = 'dataframe',
+                     ret_coef: bool = False) -> Union[pd.DataFrame,np.ndarray]:
     """
     Generate a regression dataset.
     The input parameters are the same as sklearn.datasets.make_regression
     """
 
-    X, y, _ = make_regression(n_samples=n_samples,
+    X, y, coef = make_regression(n_samples=n_samples,
                            n_features=n_features,
                            n_informative=n_informative,
                            n_targets=n_targets,
@@ -32,7 +33,10 @@ def generate_dataset(n_samples: int = 1000,
     if output == 'dataframe':
         df = pd.DataFrame(X, columns=[f'x{i}' for i in range(n_features)])
         df['target'] = y
-        return df
+        if ret_coef:
+            return df, coef
+        else:
+            return df
     elif output == 'array':
         return X, y
     else:
